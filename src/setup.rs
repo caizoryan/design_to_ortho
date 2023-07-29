95b08b0432f3c0df282599ed31dd034ab6ad52e3
Aaryan Pashine <pashineaaryan@gmail.com>
2023-07-28 23:17:23 -0400

    setup line 116 change count

--- src/setup.rs
+++ src/setup.rs
@@ -27,76 +27,76 @@ pub fn setup(
     mut images: ResMut<Assets<Image>>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    // mut export_sources: ResMut<Assets<ImageExportSource>>,
+    mut export_sources: ResMut<Assets<ImageExportSource>>,
 ) {
-    // let output_texture_handle = {
-    //     let size = Extent3d {
-    //         width: 4768,
-    //         height: 4768,
-    //         ..default()
-    //     };
-    //     let mut export_texture = Image {
-    //         texture_descriptor: TextureDescriptor {
-    //             label: None,
-    //             size,
-    //             dimension: TextureDimension::D2,
-    //             format: TextureFormat::Rgba8UnormSrgb,
-    //             mip_level_count: 1,
-    //             sample_count: 1,
-    //             usage: TextureUsages::COPY_DST
-    //                 | TextureUsages::COPY_SRC
-    //                 | TextureUsages::RENDER_ATTACHMENT
-    //                 | TextureUsages::TEXTURE_BINDING,
-    //             view_formats: &[],
-    //         },
-    //         ..default()
-    //     };
-    //     export_texture.resize(size);
-    //
-    //     images.add(export_texture)
-    // };
-    //
-    // // camera
-    // commands
-    //     .spawn((Camera3dBundle {
-    //         projection: OrthographicProjection {
-    //             scale: 350.0,
-    //             scaling_mode: ScalingMode::FixedVertical(1.0),
-    //             ..default()
-    //         }
-    //         .into(),
-    //         camera: Camera {
-    //             hdr: true,
-    //             order: -1,
-    //             target: RenderTarget::Image(output_texture_handle.clone()),
-    //             ..Default::default()
-    //         },
-    //         transform: Transform {
-    //             translation: Vec3::new(625.46, 148.73, 420.69),
-    //             rotation: Quat::from_xyzw(-0.13, 0.47, 0.23, 0.84),
-    //             ..Default::default()
-    //         },
-    //
-    //         ..Default::default()
-    //     },))
-    //     .insert(ScreenSpaceAmbientOcclusionBundle {
-    //         settings: ScreenSpaceAmbientOcclusionSettings {
-    //             quality_level: bevy::pbr::ScreenSpaceAmbientOcclusionQualityLevel::Ultra,
-    //             ..default()
-    //         },
-    //         ..default()
-    //     })
-    //     .insert(TemporalAntiAliasBundle::default());
-    //
-    // commands.spawn(ImageExportBundle {
-    //     source: export_sources.add(output_texture_handle.into()),
-    //     settings: ImageExportSettings {
-    //         // Frames will be saved to "./out/[#####].png".
-    //         output_dir: "out".into(),
-    //         // Choose "exr" for HDR renders.
-    //         extension: "png".into(),
-    //     },
-    // });
+    let output_texture_handle = {
+        let size = Extent3d {
+            width: 4768,
+            height: 4768,
+            ..default()
+        };
+        let mut export_texture = Image {
+            texture_descriptor: TextureDescriptor {
+                label: None,
+                size,
+                dimension: TextureDimension::D2,
+                format: TextureFormat::Rgba8UnormSrgb,
+                mip_level_count: 1,
+                sample_count: 1,
+                usage: TextureUsages::COPY_DST
+                    | TextureUsages::COPY_SRC
+                    | TextureUsages::RENDER_ATTACHMENT
+                    | TextureUsages::TEXTURE_BINDING,
+                view_formats: &[],
+            },
+            ..default()
+        };
+        export_texture.resize(size);
+
+        images.add(export_texture)
+    };
+
+    // camera
+    commands
+        .spawn((Camera3dBundle {
+            projection: OrthographicProjection {
+                scale: 350.0,
+                scaling_mode: ScalingMode::FixedVertical(1.0),
+                ..default()
+            }
+            .into(),
+            camera: Camera {
+                hdr: true,
+                order: -1,
+                target: RenderTarget::Image(output_texture_handle.clone()),
+                ..Default::default()
+            },
+            transform: Transform {
+                translation: Vec3::new(625.46, 148.73, 420.69),
+                rotation: Quat::from_xyzw(-0.13, 0.47, 0.23, 0.84),
+                ..Default::default()
+            },
+
+            ..Default::default()
+        },))
+        .insert(ScreenSpaceAmbientOcclusionBundle {
+            settings: ScreenSpaceAmbientOcclusionSettings {
+                quality_level: bevy::pbr::ScreenSpaceAmbientOcclusionQualityLevel::Ultra,
+                ..default()
+            },
+            ..default()
+        })
+        .insert(TemporalAntiAliasBundle::default());
+
+    commands.spawn(ImageExportBundle {
+        source: export_sources.add(output_texture_handle.into()),
+        settings: ImageExportSettings {
+            // Frames will be saved to "./out/[#####].png".
+            output_dir: "out".into(),
+            // Choose "exr" for HDR renders.
+            extension: "png".into(),
+        },
+    });
 
     commands.spawn(DirectionalLightBundle {
         transform: Transform::from_xyz(50.0, 150.0, 100.0).looking_at(Vec3::ZERO, Vec3::Y),
